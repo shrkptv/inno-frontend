cd5e966f74d78fc64681a8ce4ccabeb9e2619a63
shrkptv <alexandershirokopytov@gmail.com>
2026-02-03 23:03:46 +0300

    Add special route for user orders

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import RegisterPage from "./pages/RegisterPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import Navbar from "./components/Navbar.jsx";
+import OrdersPage from "./pages/OrdersPage.jsx";
 
 function App() {
     return (
@@ -9,6 +12,15 @@ function App() {
                 <Route path="/" element={<Navigate to="/login" />} />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/register" element={<RegisterPage />} />
+                <Route
+                    path="/orders"
+                    element={
+                        <ProtectedRoute>
+                            <Navbar />
+                            <OrdersPage />
+                        </ProtectedRoute>
+                    }
+                />
             </Routes>
         </BrowserRouter>
     );
