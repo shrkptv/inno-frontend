699ce99edc1fb6e9b030fc99611b875d9a256b4f
shrkptv <alexandershirokopytov@gmail.com>
2026-05-13 16:53:44 +0300

    Migrate Navbar to TypeScript

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,47 @@
+import {type FC} from "react";
+import { Link, useNavigate} from "react-router-dom";
+
+const Navbar: FC = () => {
+    const navigate = useNavigate();
+
+    const handleLogout = (): void => {
+        const isConfirmed = window.confirm("Are you sure you want to log out?");
+
+        if (isConfirmed) {
+            localStorage.clear();
+            navigate("/login");
+        }
+    };
+
+    return (
+        <nav className="navbar navbar-dark d-flex flex-column p-0 pb-3" style={{backgroundColor: '#6f42c1'}}>
+
+            <div className="container pt-3">
+                <div className="row w-100 align-items-center m-0">
+                    <div className="col-4"></div>
+                    <div className="col-4 text-center">
+                        <Link className="navbar-brand fw-bold fs-1 m-0" to="/orders">
+                            Inno-shop
+                        </Link>
+                    </div>
+                    <div className="col-4 text-end p-0">
+                        <button className="btn btn-outline-light px-4 fw-bold" onClick={handleLogout}>
+                            Logout
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="w-100 border-top border-light opacity-50 my-3"></div>
+
+            <div className="w-100 px-2">
+                <div className="navbar-nav d-flex flex-row gap-5 justify-content-center">
+                    <Link className="nav-link text-white fs-5 fw-medium" to="/orders">Orders</Link>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
+
